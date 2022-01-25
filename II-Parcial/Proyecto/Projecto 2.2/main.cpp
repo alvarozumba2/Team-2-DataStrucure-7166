65fecd37d584ea2e49a876f7507746971b9bf6f6
Carlos Ipiales <51092943+CarlosJoaoIpiales@users.noreply.github.com>
2022-01-25 17:37:20 -0500

    Funcion de marquesina añadida

--- II-Parcial/Proyecto/Projecto 2.2/main.cpp
+++ II-Parcial/Proyecto/Projecto 2.2/main.cpp
@@ -1,9 +1,39 @@
+#include <iostream>
+#include <thread>
+#include <Windows.h>
+
+#include "Marquee.h"
 #include "Funciones.cpp"
 
+void marquee_limit(std::string text, size_t limite) {
+    bool ciclo = true;
+    while (ciclo)
+    {
+        std::string temp = text;
+        text.erase(0, 1);
+        text += temp[0];
+        std::this_thread::sleep_for(std::chrono::milliseconds(950));
+        //poner titulo en la consola
+
+        SetConsoleTitleA(text.substr(0, limite).c_str());
+        if (GetAsyncKeyState(VK_RETURN)) {
+            ciclo = false;
+            break;
+        }
+
+    }
+}
+
 int main() {
+
     int seguir = 0;
     Funciones f;
-    system("title � Calculadora 2.0 �");
+    cout << " " << endl;
+    std::string test = "  ESTRUCTURA  DE  DATOS  7166  ";
+    std::thread t2(marquee_limit, test, test.size());
+    Marquee mark1("  AGUAS  FELIX  IPIALES  ZUMBA  ");
+    cout << " " << endl;
+    mark1.trans();
     while (seguir != ESC) {
         switch (f.menu()) {
         case '1':
